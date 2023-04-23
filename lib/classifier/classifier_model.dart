9d01cfb4f7a6b74fc570c0fd61b63c461bfc82b5
mnaqeeb06 <muhammadnaqeebawan@gmail.com>
2023-04-23 09:47:15 +0500

    making prediction

--- lib/classifier/classifier_model.dart
+++ lib/classifier/classifier_model.dart
@@ -1,3 +1,19 @@
+import 'package:tflite_flutter/tflite_flutter.dart';
+
 class ClassifierModel {
-  // TODO: Add model parameters
+  Interpreter interpreter;
+
+  List<int> inputShape;
+  List<int> outputShape;
+
+  TfLiteType inputType;
+  TfLiteType outputType;
+
+  ClassifierModel({
+    required this.interpreter,
+    required this.inputShape,
+    required this.outputShape,
+    required this.inputType,
+    required this.outputType,
+  });
 }
